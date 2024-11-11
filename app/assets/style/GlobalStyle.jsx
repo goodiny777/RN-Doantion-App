e83fe78cd167343148e962152e9409c63b2213c7
Misha <goodiny777@gmial.com>
2024-11-11 06:59:09 -0700

    modified folders structure Added global style Added font helper

---
+++ app/assets/style/GlobalStyle.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+
+import { getFontFamily } from '../../helpers/FontsHelper';
+
+const style = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    messageIcon: {
+        padding: 14,
+        backgroundColor: '#F9FAFB',
+        borderRadius: 50,
+    },
+    messageBadgeContainer: {
+        backgroundColor: '#F35BAC',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        width: 10,
+        height: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        position: 'absolute',
+        right: 10,
+        top: 10,
+    },
+    messageBadge: {
+        color: '#FFFFFF',
+        fontSize: 6,
+        fontFamily: getFontFamily('Inter', '600'),
+    },
+});
+
+export default style;
