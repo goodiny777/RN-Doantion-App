63705abc634f46767d83e32cd4cf6d61f1012571
Misha <goodiny777@gmial.com>
2024-11-23 14:52:46 -0700

    search component done

---
+++ app/presentation/components/search/Search.js
@@ -0,0 +1,45 @@
+import { React, useRef, useState } from 'react';
+import { Pressable, Text, View } from 'react-native';
+
+import style from './style';
+import PropTypes from 'prop-types';
+import { horizontalScale, scaleFontSize } from '../../../helpers/Scaling';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { TextInput } from 'react-native-gesture-handler';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Search = props => {
+    const textInputRef = useRef(null);
+    const [search, setSearch] = useState('');
+
+    const handleFocus = () => {
+        textInputRef.current.focus();
+    };
+
+    const handleSearch = (value) => {
+        setSearch(value);
+        props.onTextChanged(value);
+    };
+    return (
+        <Pressable style={style.searchInputContainter} onPress={handleFocus}>
+            <FontAwesomeIcon
+                icon={faSearch}
+                color="#25C0FF"
+                size={scaleFontSize(22)}
+            />
+            <TextInput
+                ref={textInputRef}
+                style={style.searchInput}
+                value={search}
+                onChangeText={(value) => handleSearch(value)}
+            />
+        </Pressable>
+    );
+};
+
+Search.propTypes = {
+    title: PropTypes.string.isRequired,
+    onTextChanged: PropTypes.func,
+};
+
+export default Search;
---
+++ app/presentation/components/search/style.js
@@ -0,0 +1,28 @@
+
+import { StyleSheet } from 'react-native';
+
+import { getFontFamily } from '../../../helpers/FontsHelper';
+import { horizontalScale, scaleFontSize, verticalScale } from '../../../helpers/Scaling';
+
+const style: StyleSheet = StyleSheet.create({
+    searchInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: horizontalScale(16),
+        backgroundColor: '#F3F5F9',
+        height: verticalScale(50),
+        borderRadius: horizontalScale(15),
+    },
+    searchInput: {
+        flex: 1,
+        marginLeft: horizontalScale(6),
+        height: '100%',
+        fontFamily: 'Inter',
+        fontSize: scaleFontSize(14),
+        lineHeight: scaleFontSize(14),
+        color: '#686C7A',
+
+    },
+});
+
+export default style;
