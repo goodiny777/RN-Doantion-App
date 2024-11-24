0c92fc5fc7faf3bdb30b2bf9f547adc84af152b4
Misha <goodiny777@gmial.com>
2024-11-24 08:13:52 -0700

    adnroid rgadle plugin fixed MainActivity fixed

--- android/app/src/main/java/com/donationapp/MainActivity.kt
+++ android/app/src/main/java/com/donationapp/MainActivity.kt
@@ -8,10 +8,10 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  override fun onCreate(savedInstanceState: Bunle){
-    super.onCreate(null)
-  }
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
