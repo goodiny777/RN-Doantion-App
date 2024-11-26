1d8586795d5e40eff5839f88200ed805833024ab
Misha <goodiny777@gmial.com>
2024-11-26 01:25:03 -0700

    added persist storage

--- app/App.js
+++ app/App.js
@@ -3,16 +3,19 @@ import React from 'react';
 import MainNavigator from './navigation/MainNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 import { View } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer screenOptions={{ header: () => null, headerShown: false }}>
-        <MainNavigator />
-      </NavigationContainer>
+      <PersistGate persistor={persistor} loading={null} >
+        <NavigationContainer screenOptions={{ header: () => null, headerShown: false }}>
+          <MainNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,16 +1,26 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import User from './reducers/User';
 import { logger } from "redux-logger";
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
 
 
 const rootReducer = combineReducers({
     user: User,
 });
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    version: 1,
+};
+const persisterReducer = persistReducer(config, rootReducer);
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persisterReducer,
     middleware: defaultMidleware => {
-        return defaultMidleware().concat(logger);
+        return defaultMidleware({ serializableCheck: false }).concat(logger);
     }
 });
 
 export default store;
+
+export const persistor = persistStore(store);
